928c2147f2db8eb7cda2e735c7b7398e42ccd89f
harshavardhan.r <harshavardhan.r@GSPSFJ9S542>
2024-12-31 13:01:42 +0530

    Add project files.

---
+++ Employee.DAL/EmployeeDAL.cs
@@ -0,0 +1,112 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using Employee.Domain;
+using System.Data;
+using System;
+
+namespace Employee.DAL
+{
+    public class EmployeeDAL
+    {
+        public SqlConnection _connection { set; get; }
+        private SqlCommand _command;
+        public List<EmployeeModel> Employees { get; set; }
+        public EmployeeDAL()
+        {
+            _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeCon"].ConnectionString);
+            Employees = new List<EmployeeModel>();
+        }
+        public List<EmployeeModel> GetEmployees()
+        {
+            Employees.Clear();
+            _command = new SqlCommand("select * from Employee", _connection);
+            _connection.Open();
+            SqlDataReader dr =  _command.ExecuteReader();
+            if(dr.HasRows)
+            {
+                while(dr.Read())
+                {
+                    EmployeeModel emp = new EmployeeModel()
+                    {
+                        Eno = Convert.ToInt32(dr["Eno"]),
+                        Ename = Convert.ToString(dr["Ename"]),
+                        Job = Convert.ToString(dr["Job"]),
+                        Salary = Convert.ToDouble(dr["Salary"]),
+                        Dname = Convert.ToString(dr["Dname"])
+                    };
+                    Employees.Add(emp);
+                }
+                _connection.Close();
+                return Employees;
+            }
+            return new List<EmployeeModel>();
+        }
+        public string InsertEmployee(EmployeeModel employee)
+        {
+            string query = $"insert into Employee values({employee.Eno},'{employee.Ename}','{employee.Job}',{employee.Salary},'{employee.Dname}')";
+            _command = new SqlCommand(query, _connection);
+            _connection.Open();
+            SqlDataReader dr = _command.ExecuteReader();
+            string msg = dr.RecordsAffected+" Record(s) Affected";
+            _connection.Close();
+            return msg;
+        }
+        public string UpdateEmployee(int eno)
+        {
+            Console.WriteLine("Enter Employee Number (Eno): ");
+            int enom = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter new Employee Name (Ename): ");
+            string ename = Console.ReadLine();
+            Console.WriteLine("Enter new Job: ");
+            string job = Console.ReadLine();
+            Console.WriteLine("Enter new Salary: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter new Department Name (Dname): ");
+            string dname = Console.ReadLine();
+            _command = new SqlCommand("usp_update_employee", _connection);
+            _command.CommandType = CommandType.StoredProcedure;
+            _command.Parameters.AddWithValue("@eno", enom);
+            _command.Parameters.AddWithValue("@ename", ename);
+            _command.Parameters.AddWithValue("@job", job);
+            _command.Parameters.AddWithValue("@salary", salary);
+            _command.Parameters.AddWithValue("@dname", dname);
+            _connection.Open();
+            string result = _command.ExecuteScalar().ToString();
+            _connection.Close();
+            return result;
+        }
+        public string DeleteEmployee(EmployeeModel employee)
+        {
+            _command = new SqlCommand("usp_delete_employee", _connection);
+            _command.CommandType = CommandType.StoredProcedure;
+            _command.Parameters.AddWithValue("@eno", employee.Eno);
+            _connection.Open();
+            string result = _command.ExecuteScalar().ToString();
+            _connection.Close();
+            return result;
+        }
+        public List<EmployeeModel> SortEmployee(string columnName, string order)
+        {
+            List<EmployeeModel> sortedEmployees = new List<EmployeeModel>();
+            string query = $"select * from employee order by {columnName} {order}";
+            _command = new SqlCommand(query, _connection);
+            _connection.Open();
+            SqlDataReader dr = _command.ExecuteReader();
+            while (dr.Read())
+            {
+                EmployeeModel emp = new EmployeeModel()
+                {
+                    Eno = Convert.ToInt32(dr["Eno"]),
+                    Ename = Convert.ToString(dr["Ename"]),
+                    Job = Convert.ToString(dr["Job"]),
+                    Salary = Convert.ToDouble(dr["Salary"]),
+                    Dname = Convert.ToString(dr["Dname"])
+                };
+                sortedEmployees.Add(emp);
+            }
+            _connection.Close();
+            return sortedEmployees;
+        }
+    }
+}
---
+++ Employee.UI/Program.cs
@@ -0,0 +1,134 @@
+﻿using Employee.BAL;
+using Employee.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Employee.UI
+{
+    internal class Program
+    {
+        static EmployeeBAL objBAL;
+        static Program()
+        {
+            objBAL = new EmployeeBAL();
+        }
+        static EmployeeModel GetInputEmp()
+        {
+            Console.WriteLine("Enter Employee Details: ");
+            return new EmployeeModel()
+            {
+                Eno = Convert.ToInt32(Console.ReadLine()),
+                Ename = Console.ReadLine(),
+                Job = Console.ReadLine(),
+                Salary = Convert.ToDouble(Console.ReadLine()),
+                Dname = Console.ReadLine()
+            };
+        }
+        static string InsertEmployee()
+        {
+            EmployeeModel emp = GetInputEmp();
+            string res = objBAL.InsertEmployee(emp);
+            Console.WriteLine(res);
+            Console.WriteLine("Updated Table: ");
+            List<EmployeeModel> emps = objBAL.GetEmployees();
+            Display(emps);
+            return res;
+        }
+        static string UpdateEmployee()
+        {
+            Console.WriteLine("Enter Employee Eno to Update: ");
+            int eno = Convert.ToInt32(Console.ReadLine());
+            string res = objBAL.UpdateEmployee(eno);
+            Console.WriteLine(res);
+            Console.WriteLine("Updated Table: ");
+            List<EmployeeModel> emps = objBAL.GetEmployees();
+            Display(emps);
+            return res;
+        }
+        static string DeleteEmployee()
+        {
+            Console.WriteLine("Enter Employee Eno to Delete: ");
+            int eno = Convert.ToInt32(Console.ReadLine());
+            EmployeeModel emp = new EmployeeModel() { Eno = eno };
+            string res = objBAL.DeleteEmployee(emp);
+            Console.WriteLine(res);
+            Console.WriteLine("Updated Table: ");
+            List<EmployeeModel> emps = objBAL.GetEmployees();
+            Display(emps);
+            return res;
+        }
+        static void SortEmployee()
+        {
+            Console.WriteLine("Select column to sort by: ");
+            Console.WriteLine("1. Eno\n2. Ename\n3. Job\n4. Salary\n5. Dname");
+            int columnChoice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select sorting order: ");
+            Console.WriteLine("1. Ascending\n2. Descending");
+            int orderChoice = Convert.ToInt32(Console.ReadLine());
+            string columnName = "";
+            switch (columnChoice)
+            {
+                case 1:
+                    columnName = "Eno";
+                    break;
+                case 2:
+                    columnName = "Ename";
+                    break;
+                case 3:
+                    columnName = "Job";
+                    break;
+                case 4:
+                    columnName = "Salary";
+                    break;
+                case 5:
+                    columnName = "Dname";
+                    break;
+                default:
+                    columnName = "Eno";
+                    break;
+            }
+            string order = orderChoice == 1 ? "ASC" : "DESC";
+            List<EmployeeModel> sortedEmployees = objBAL.SortEmployee(columnName, order);
+            Console.WriteLine("Sorted Table: ");
+            Display(sortedEmployees);
+            Console.WriteLine("Table sorted successfully.");
+        }
+
+        static void Display(List<EmployeeModel> employees)
+        {
+            Console.WriteLine("{0,-10} {1,-20} {2,-20} {3,-10} {4,-20}", "Eno", "Ename", "Job", "Salary", "Dname");
+            Console.WriteLine(new string('-', 75));
+            foreach (EmployeeModel e in employees)
+            {
+                Console.WriteLine("{0,-10} {1,-20} {2,-20} {3,-10} {4,-20}", e.Eno, e.Ename, e.Job, e.Salary, e.Dname);
+            }
+        }
+        static void Main(string[] args)
+        {            
+            List<EmployeeModel> emps = objBAL.GetEmployees();
+            Display(emps);
+            Console.WriteLine("\nSelect operation: ");
+            Console.WriteLine("1.Insert\n2.Update\n3.Delete\n4.SortTable");
+            int dbChoice = Convert.ToInt32(Console.ReadLine());
+            switch (dbChoice)
+            {
+                case 1:
+                    InsertEmployee();
+                    break;
+                case 2:
+                    UpdateEmployee();
+                    break;
+                case 3:
+                    DeleteEmployee();
+                    break;
+                case 4:
+                    SortEmployee();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+            Console.Read();
+        }        
+    }
+}
